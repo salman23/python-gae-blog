cbb36db1d0f6cd84df9bf2fa6fa2353754c432b3
Salman Wahed <salman2312@gmail.com>
2014-06-24 20:33:14 +0600

    google app engine project

---
+++ mypage.py
@@ -0,0 +1,12 @@
+__author__ = 'algonyx'
+
+import webapp2
+
+
+class MainPage(webapp2.RedirectHandler):
+    def get(self, *args, **kwargs):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, I am Salman Wahed')
+
+
+application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
