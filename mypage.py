4d889a90039a0b4338077a48e38ab659b9e3ead7
Salman Wahed <salman2312@gmail.com>
2014-07-11 16:15:40 +0600

    gae blog

--- mypage.py
+++ mypage.py
@@ -1,12 +1,27 @@
 __author__ = 'salman wahed'
 
 import webapp2
+import os
+import jinja2
 
 
-class MainPage(webapp2.RedirectHandler):
-    def get(self, *args, **kwargs):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, I am Salman Wahed')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kwargs):
+        self.response.out.write(*args, **kwargs)
+
+    def render_str(self, template, **kwargs):
+        t = jinja_env.get_template(template)
+        return t.render(kwargs)
+
+    def render(self, template, **kwargs):
+        self.write(self.render_str(template, **kwargs))
+
+class MainPage(Handler):
+    def get(self):
+        self.render('shoping_list.html')
 
 application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
